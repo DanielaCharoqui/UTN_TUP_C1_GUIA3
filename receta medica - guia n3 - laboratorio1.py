c9b8259ccf219015ab9b16232c39848ac7b9b3ff
DanielaCharoqui <143364269+DanielaCharoqui@users.noreply.github.com>
2023-09-04 00:35:56 -0300

    charoqui_comision1

---
+++ receta medica - guia n3 - laboratorio1.py
@@ -0,0 +1,27 @@
+print("CENTRO DE REHABILITACION LUZ")
+print("Direccion: San Juan 1184, Villa María, Cordoba")
+print("TELEFONO: 0353 461-1813")
+
+print("RECETA MEDICA")
+
+nombrepaciente= input("Por favor, ingrese nombre del paciente ")
+nacimiento= input("Por favor, ingrese fecha de nacimiento del paciente ")
+OS= input("Por favor, ingrese obra social del paciente ")
+medicamento= input("Por favor, ingrese medicamento recetado ")
+dosis= input("Por favor, ingrese dosis ")
+instrucciones= input("Por favor, ingrese instrucciones de uso ")
+medico= input("Por favor, ingrese nombre del medico ")
+fecha= input("Por favor, ingrese fecha actual ")
+
+proxturno= input("Por favor, ingrese fecha del proximo turno ")
+
+print("NOMBRE DEL PACIENTE: ", nombrepaciente)
+print("FECHA DE NACIMIENTO: ", nacimiento)
+print("OBRA SOCIAL DEL PACIENTE: ", OS)
+print("MEDICAMENTO: ", medicamento)
+print("DOSIS: ", dosis)
+print("INSTRUCCIONES : ", instrucciones)
+print("MEDICO: ", medico)
+print("FECHA ACTUAL: ", fecha)
+
+print("FECHA DEL PROXIMO TURNO: ", proxturno)
